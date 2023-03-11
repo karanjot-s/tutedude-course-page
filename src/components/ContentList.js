12acb57790bfccb543b1e745d5d39890bb01e011
Karanjot Singh <66475280+karanjot-s@users.noreply.github.com>
2023-03-12 01:43:10 +0530

    removed warnings

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles/ContentList.module.css";
 import ContentListItem from "./ContentListItem";
 
--- src/components/VideoCont.js
+++ src/components/VideoCont.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "../styles/VideoCont.module.css";
 
 const VideoCont = ({ src, main, sub }) => {
